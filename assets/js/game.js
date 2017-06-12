ce2f122bd5299e591e30b0a4d1adb8e9584236b1
Ana Durand <ana.dc1004@gmail.com>
2017-06-12 12:05:45 -0500

    agregando animacion fade

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,7 +8,7 @@ const Game = (update) => {
   const label = $('<label for="name" class="label">Ingresa su nombre:</label>');
   const input = $('<input type="text" placeholder="Nombre" class="input">');
   const button = $('<button type="button" name="button" class="gold boton">Comprobar</button>');
-  const alert = $('<p class="alert"></p>');
+  const alerta = $('<p class="alert"></p>');
   const puntaje = $("#score");
   let score = 0;
 
@@ -16,46 +16,48 @@ const Game = (update) => {
   formContainer.append(label);
   formContainer.append(input);
   formContainer.append(button);
-  formContainer.append(alert);
+  formContainer.append(alerta);
   parent.append(imgContainer);
   parent.append(formContainer);
 
-  let number = random(foto);
+  let number = random(foto, alerta);
   let chance = 0;
   button.on("click", function(){
-    console.log(number);
     console.log(state.selectedSede[0].students[number].name);
 
     if(input.val().toLowerCase() == state.selectedSede[0].students[number].name.toLowerCase()){
-      alert.text("Excelente, Acertaste");
+      alerta.text("Excelente, Acertaste");
       chance = 0;
       score += 5;
-      console.log(arrarNumber.length);
+      foto.addClass("fade");
+
       if(arrarNumber.length == state.selectedSede[0].students.length){
         state.status = "over";
         arrarNumber = [];
-        console.log(arrarNumber);
         update();
       }else{
         setTimeout(function(){
-          number = random(foto);
-          console.log(number);
+          number = random(foto, alerta);
+          foto.removeClass("fade");
           return number;
         },2000);
       }
     }else{
       chance += 1;
-      alert.text("Sigue Intentando");
+      alerta.text("Sigue Intentando");
       if(chance == 5){
         chance = 0;
         score -= 1;
+        foto.addClass("fade");
+        
         if(arrarNumber.length == state.selectedSede[0].students.length){
           state.staus = "over";
           arrarNumber = [];
           update();
         }else{
           setTimeout(function(){
-            return number = random(foto)},2000);
+            foto.removeClass("fade");
+            return number = random(foto, alerta);},2000);
         }
       }
     }
@@ -69,7 +71,7 @@ const Game = (update) => {
 
 let arrarNumber=[];
 
-const random = (foto) => {
+const random = (foto, alerta) => {
     const numero = parseInt(Math.random() * (state.selectedSede[0].students.length));
     const actualNumber = arrarNumber.find(function(value){
                             return value == numero;
@@ -77,11 +79,9 @@ const random = (foto) => {
     if(actualNumber == undefined){
         foto.attr("src", "assets/img/"+ state.selectedSede[0].name + "/" + state.selectedSede[0].students[numero].image);
         arrarNumber.push(numero);
-      console.log(numero);
     }else{
-      console.log(numero);
       random(foto);
     }
-
+    alerta.text("");
     return arrarNumber[arrarNumber.length-1];
 }
--- assets/js/mexico.js
+++ assets/js/mexico.js
@@ -13,93 +13,91 @@ var mexico = [{
             },{
               image: "BeatrizQuesadas.JPG",
               name: "Beatriz"
-            }]
-
-            // ,{
-            //   image: "ClaudiaHernandez.JPG",
-            //   name: "Claudia"
-            // },{
-            //   image: "DanielaBelemGarcia.JPG",
-            //   name: "Daniela"
-            // },{
-            //   image: "ElisaGuadalupeMartinez.JPG",
-            //   name: "Elisa"
-            // },{
-            //   image: "EvelynVazquez.JPG",
-            //   name: "Evelyn"
-            // },{
-            //   image: "GabrielaPenaloza.JPG",
-            //   name: "Gabriela"
-            // },{
-            //   image: "GrisselCoutino.JPG",
-            //   name: "Grissel"
-            // },{
-            //   image: "GuadalupeGuerrero.JPG",
-            //   name: "Guadalupe"
-            // },{
-            //   image: "JohanaAlexaVargas.JPG",
-            //   name: "Johana"
-            // },{
-            //   image: "JoyceZeltzinHernandez.JPG",
-            //   name: "Joyce"
-            // },{
-            //   image: "JuanaOfeliaGarcia.JPG",
-            //   name: "Juana"
-            // },{
-            //   image: "KarenA.SharonDeDiego.JPG",
-            //   name: "Karen"
-            // },{
-            //   image: "KarenCruzHeredia.JPG",
-            //   name: "Karen Cruz"
-            // },{
-            //   image: "KarenQuiroz.JPG",
-            //   name: "Karen Quiroz"
-            // },{
-            //   image: "KarlaMonicaLlerenas.JPG",
-            //   name: "Karla"
-            // },{
-            //   image: "KarlaVargas.JPG",
-            //   name: "Karla Vargas"
-            // },{
-            //   image: "LeslieAnigailVazquez.JPG",
-            //   name: "Leslie"
-            // },{
-            //   image: "LilianMishelMartinez.JPG",
-            //   name: "Lilian"
-            // },{
-            //   image: "MilcaSaraiDelAngel.JPG",
-            //   name: "Milca"
-            // },{
-            //   image: "NaibitLeonel.JPG",
-            //   name: "Naibit"
-            // },{
-            //   image: "NayeliCarbajal.JPG",
-            //   name: "Nayeli"
-            // },{
-            //   image: "NellyMontserratSaavedra.JPG",
-            //   name: "Nelly"
-            // },{
-            //   image: "ReynaHernandez.JPG",
-            //   name: "Reyna"
-            // },{
-            //   image: "RubiAdrianaSantiago.JPG",
-            //   name: "Rubi"
-            // },{
-            //   image: "RuthLopez.JPG",
-            //   name: "Ruth"
-            // },{
-            //   image: "RuthZacnicteVega.JPG",
-            //   name: "Ruth Zacnicte"
-            // },{
-            //   image: "SandraBollo.JPG",
-            //   name: "Sandra"
-            // },{
-            //   image: "SandraDiaz.JPG",
-            //   name: "Sandra Diaz"
-            // },{
-            //   image: "VianeyTavathaMoreno.JPG",
-            //   name: "Vianey"
-            // },{
-            //   image: "ZazilAuroraMartinez.JPG",
-            //   name: "Zazil"
-            // }];
+            },{
+              image: "ClaudiaHernandez.JPG",
+              name: "Claudia"
+            },{
+              image: "DanielaBelemGarcia.JPG",
+              name: "Daniela"
+            },{
+              image: "ElisaGuadalupeMartinez.JPG",
+              name: "Elisa"
+            },{
+              image: "EvelynVazquez.JPG",
+              name: "Evelyn"
+            },{
+              image: "GabrielaPenaloza.JPG",
+              name: "Gabriela"
+            },{
+              image: "GrisselCoutino.JPG",
+              name: "Grissel"
+            },{
+              image: "GuadalupeGuerrero.JPG",
+              name: "Guadalupe"
+            },{
+              image: "JohanaAlexaVargas.JPG",
+              name: "Johana"
+            },{
+              image: "JoyceZeltzinHernandez.JPG",
+              name: "Joyce"
+            },{
+              image: "JuanaOfeliaGarcia.JPG",
+              name: "Juana"
+            },{
+              image: "KarenA.SharonDeDiego.JPG",
+              name: "Karen"
+            },{
+              image: "KarenCruzHeredia.JPG",
+              name: "Karen Cruz"
+            },{
+              image: "KarenQuiroz.JPG",
+              name: "Karen Quiroz"
+            },{
+              image: "KarlaMonicaLlerenas.JPG",
+              name: "Karla"
+            },{
+              image: "KarlaVargas.JPG",
+              name: "Karla Vargas"
+            },{
+              image: "LeslieAnigailVazquez.JPG",
+              name: "Leslie"
+            },{
+              image: "LilianMishelMartinez.JPG",
+              name: "Lilian"
+            },{
+              image: "MilcaSaraiDelAngel.JPG",
+              name: "Milca"
+            },{
+              image: "NaibitLeonel.JPG",
+              name: "Naibit"
+            },{
+              image: "NayeliCarbajal.JPG",
+              name: "Nayeli"
+            },{
+              image: "NellyMontserratSaavedra.JPG",
+              name: "Nelly"
+            },{
+              image: "ReynaHernandez.JPG",
+              name: "Reyna"
+            },{
+              image: "RubiAdrianaSantiago.JPG",
+              name: "Rubi"
+            },{
+              image: "RuthLopez.JPG",
+              name: "Ruth"
+            },{
+              image: "RuthZacnicteVega.JPG",
+              name: "Ruth Zacnicte"
+            },{
+              image: "SandraBollo.JPG",
+              name: "Sandra"
+            },{
+              image: "SandraDiaz.JPG",
+              name: "Sandra Diaz"
+            },{
+              image: "VianeyTavathaMoreno.JPG",
+              name: "Vianey"
+            },{
+              image: "ZazilAuroraMartinez.JPG",
+              name: "Zazil"
+            }];
